ac47bc694598e045f7097b9648ac9c13bdb0c929
Sekhar Sankaramanchi <sekhar@isovalent.com>
2025-08-10 09:00:42 -0700

    chore: update terraform version

--- versions.tf
+++ versions.tf
@@ -4,12 +4,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.62.0"
+      version = ">= 3.62.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-}
+provider "azurerm" {}
